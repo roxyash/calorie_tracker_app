99a48a8583c2873515c511e755aaa17628c17dcf
roxyash <roxyash@mail.ru>
2022-10-15 17:47:19 +0600

    implement getEntries

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -19,7 +19,27 @@ func AddEntry(c *gin.Context) {
 }
 
 func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+	var  entries []bson.M
+
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err := cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
 }
 
 func GetEntriesByIngredient(c *gin.Context) {
@@ -53,7 +73,7 @@ func DeleteEntry(c *gin.Context) {
 	}
 
 	defer cancel()
-	c.JSON(http.StatusOK, result.DeletedCount}) 
+	c.JSON(http.StatusOK, result.DeletedCount) 
 
 
 }
