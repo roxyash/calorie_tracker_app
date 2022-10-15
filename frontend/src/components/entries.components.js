3e6e059b40d91ee6cc61fb6f8799cd84040d75f5
roxyash <roxyash@mail.ru>
2022-10-15 19:42:48 +0600

    create and implement two methods addSingleEntry, deleteSingleEntry

--- frontend/src/components/entries.components.js
+++ frontend/src/components/entries.components.js
@@ -6,4 +6,47 @@ import {Button, Form, Container, Modal} from 'react-bootstrap';
 
 import Entry from './single-entry.components';
 
+const Entries =() =>{
 
+    return(
+        <div>
+            <Container>
+                <Button onClick={()=> setAddNewEntry(true)}>Track today`s calories</Button>
+
+            </Container>
+            <Container>
+                {entries != null && entries.map((entry, i)=> (
+                    <Entry entryData={entry} deleteSingleEntry={deleteSingleEntry} setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry}/>
+                ))}                
+            </Container>
+        </div>
+    );
+}
+
+
+function addSingleEntry(){
+    setAddNewEntry(false)
+    var url = "http://localhost:8000/entry/create"
+    axios.post(url,{
+        "ingredients": newEntry.ingredients,
+        "dish": newEntry.dish,
+        "calories": newEntry.calories,
+        "fat": parseFloat(newEntry.fat)
+    }).then(response => {
+        if (response.status == 200) {
+            setRefreshData(true)
+        }
+    })
+}
+
+
+function deleteSingleEntry(id){
+    var url = "http://localhost:8000/entry/delete" + id
+    axios.delete(url, {
+
+    }).then(response) => {
+        if (response.status == 200) {
+            setRefreshData(true)
+        }
+    }
+}
