4fe9a75b816ad9168d9763c2b6320cc596d32581
roxyash <roxyash@mail.ru>
2022-10-15 20:03:17 +0600

    create modal

--- frontend/src/components/entries.components.js
+++ frontend/src/components/entries.components.js
@@ -19,34 +19,62 @@ const Entries =() =>{
                     <Entry entryData={entry} deleteSingleEntry={deleteSingleEntry} setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry}/>
                 ))}                
             </Container>
+
+            <Modal show={addNewEntry} onHide={() => setAddNewEntry(false)} centred>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Add Calorie Entry 
+                    </Modal.Title>
+
+                    <Modal.Body>
+                        <Form.Group>
+                            <Form.Label>dish</Form.Label>
+                            <Form.Control onChange={(event) => {newEntry.dish = event.target.value}}></Form.Control>
+                            <Form.Label>ingredients</Form.Label>
+                            <Form.Control onChange={(event) => {newEntry.ingredients = event.target.value}}></Form.Control>
+                            <Form.Label>calories</Form.Label>
+                            <Form.Control onChange={(event) => {newEntry.calories = event.target.value}}></Form.Control>
+                            <Form.Label>fat</Form.Label>
+                            <Form.Control onChange={(event) => {newEntry.fat = event.target.value}}></Form.Control>
+                        </Form.Group>
+                        <Button onClick={() => addSingleEntry()}>Add</Button>
+                        <Button onClick={() => setAddNewEntry(false)}>Cancel</Button>
+                    </Modal.Body>
+
+                </Modal.Header>
+
+            </Modal>
         </div>
     );
-}
 
-
-function addSingleEntry(){
-    setAddNewEntry(false)
-    var url = "http://localhost:8000/entry/create"
-    axios.post(url,{
-        "ingredients": newEntry.ingredients,
-        "dish": newEntry.dish,
-        "calories": newEntry.calories,
-        "fat": parseFloat(newEntry.fat)
-    }).then(response => {
-        if (response.status == 200) {
-            setRefreshData(true)
-        }
-    })
-}
-
-
-function deleteSingleEntry(id){
-    var url = "http://localhost:8000/entry/delete" + id
-    axios.delete(url, {
-
-    }).then(response) => {
-        if (response.status == 200) {
-            setRefreshData(true)
+    function addSingleEntry(){
+        setAddNewEntry(false)
+        var url = "http://localhost:8000/entry/create"
+        axios.post(url,{
+            "ingredients": newEntry.ingredients,
+            "dish": newEntry.dish,
+            "calories": newEntry.calories,
+            "fat": parseFloat(newEntry.fat)
+        }).then(response => {
+            if (response.status == 200) {
+                setRefreshData(true)
+            }
+        })
+    }
+    
+    
+    function deleteSingleEntry(id){
+        var url = "http://localhost:8000/entry/delete" + id
+        axios.delete(url, {
+    
+        }).then(response) => {
+            if (response.status == 200) {
+                setRefreshData(true)
+            }
         }
     }
 }
+
+
+
+ 
